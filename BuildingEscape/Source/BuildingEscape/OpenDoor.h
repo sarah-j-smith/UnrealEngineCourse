6f25a4fdb511066fb3e701e33be3220aa40c70ea
Sarah Smith <sarah@smithsoft.com.au>
2017-01-30 23:14:49 +1000

    Add materials and begin on properties.

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -21,6 +21,8 @@ public:
 	// Called every frame
 	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
 
-		
+private:
+    UPROPERTY(VisibleAnywhere)
+    float OpenAngle = -90.0f;
 	
 };
